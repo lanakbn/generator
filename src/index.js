d5994779d44f016b812925842f665d14dabb4d2e
lanakbn <lana.kbn@gmail.com>
2025-09-23 23:01:03 +0200

    improved css

--- src/index.js
+++ src/index.js
@@ -14,9 +14,12 @@ function generateQuote(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "ta22c25ab23c6669019e2c2b2ocf4595";
 
-  let context = `You are a creative marketer who specializes in crafting catchy, highly specific slogans for products or services. Generate a short, memorable, and appealing slogan ONLY about this topic: ${instructionsInput}. Focus exactly on the user’s topic and instructions. Avoid generic themes unrelated to the product. Important rules: Focus ONLY on this topic (do not mention general ideas like innovation, future, success, etc., unless directly relevant).   the slogan must be directly about the product/service itself.  Keep it under 10 words.  Make it sound appealing and specific to the audience.At the end of the quote, sign it with "SheCodes AI" inside a <strong>`;
-  let prompt = ` Generate a marketing quote about the topic of ${instructionsInput}.  `;
+  let context = `You are a creative marketer who specializes in crafting catchy, specific slogans for products or services. Generate a short, memorable, and appealing slogan ONLY about this topic: ${instructionsInput}. Focus exactly on the user’s topic and instructions. Avoid generic themes unrelated to the product. Important rules: Focus ONLY on this topic (do not mention general ideas like innovation, future, success, etc., unless directly relevant).   the slogan must be directly about the product/service itself.  Keep it under 10 words.  Make it sound appealing and specific to the audience.At the end of the quote, sign it with "SheCodes AI" inside a <strong>`;
+  let prompt = ` Generate a marketing quote about the topic of ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let quoteEl = document.querySelector("#marketQuote");
+  quoteEl.classList.remove("hidden");
+  quoteEl.innerHTML = ` <span class= "generating"> Generating a quote </span>`;
 
   console.log("Generating quote...");
 
